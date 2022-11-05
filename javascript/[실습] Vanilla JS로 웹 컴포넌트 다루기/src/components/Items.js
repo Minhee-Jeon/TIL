3cba315f9d4e91e9f98d8450ee5d9d9484d8bed1
minhee <jmh3699@naver.com>
2022-11-05 16:54:09 +0900

    2-1 [컴포넌트 분할하기] 기능 추가
    `Items` 컴포넌트에 `toggle`, `filter`의 기능을 추가했을 때의 문제점:
    코드를 관리하기 힘들고, 재활용이 목적인 컴포넌트 단위로 활용할 수 없는 상태가 되어버렸다.
    => 컴포넌트 하나가 최대한 작은 단위의 일을 하도록 만들어야 한다.

--- javascript/[실습] Vanilla JS로 웹 컴포넌트 다루기/src/components/Items.js
+++ javascript/[실습] Vanilla JS로 웹 컴포넌트 다루기/src/components/Items.js
@@ -1,33 +1,89 @@
 import Component from '../core/component.js';
 
 export default class Items extends Component {
-  setup () {
-    this.$state = { items: ['item1', 'item2'] };
+  get filteredItems () {
+    const { isFilter, items } = this.$state;
+    return items.filter(({ active }) => (isFilter === 1 && active) ||
+                                        (isFilter === 2 && !active) ||
+                                        isFilter === 0);
   }
+
+  setup () {
+    this.$state = {
+      ifFilter: 0,
+      items: [
+        {
+          seq: 1,
+          contents: 'item1',
+          active: false,
+        },
+        {
+          seq: 1,
+          contents: 'item2',
+          active: true,
+        }
+      ]
+    };
+  }
+
   template () {
-    const { items } = this.$state;
     return `
-      <ul>
-      ${items.map((item, key) => `
-        <li>
-          ${item}
-          <button class="deleteBtn" data-index="${key}">삭제</button>
-        </li>
-      `).join('')}
-    </ul>
-    <button class="addBtn">추가</button>
+      <header>
+        <input type="text" class="appender" placeholder="아이템 내용 입력" />
+      </header>
+      <main>
+        <ul>
+          ${this.filteredItems.map(({contents, active, seq}) => `
+            <li data-seq="${seq}">
+              ${contents}
+              <button class="toggleBtn" style="color: ${active ? '#09F' : '#F09'}">
+                ${active ? '활성' : '비활성'}
+              </button>
+              <button class="deleteBtn">삭제</button>
+            </li>
+          `).join('')}
+        </ul>
+      </main>
+      <footer>
+        <button class="filterBtn" data-is-filter="0">전체 보기</button>
+        <button class="filterBtn" data-is-filter="1">활성 보기</button>
+        <button class="filterBtn" data-is-filter="2">비활성 보기</button>
+      </footer>
     `
   }
 
   setEvent () {
-    this.addEvent('click', '.addBtn', ({ target }) => {
-      const { items } = this.$state;
-      this.setState({ items: [ ...items, `item${items.length + 1}` ] });
+    this.addEvent('keyup', '.appender', ({ key, target }) => {
+      if (key !== 'Enter') return;
+      const {items} = this.$state;
+      const seq = Math.max(0, ...items.map(v => v.seq)) + 1;
+      const contents = target.value;
+      const active = false;
+      this.setState({
+        items: [
+          ...items,
+          {seq, contents, active}
+        ]
+      });
     });
+
     this.addEvent('click', '.deleteBtn', ({ target }) => {
       const items = [ ...this.$state.items ];
-      items.splice(target.dataset.index, 1);
+      const seq = Number(target.closest('[data-seq]').dataset.seq);
+      items.splice(items.findIndex(v => v.seq === seq), 1);
       this.setState({ items });
+    });
+
+    this.addEvent('click', '.toggleBtn', ({ target }) => {
+      const items = [...this.$state.items];
+      const seq = Number(target.closest('[data-seq]').dataset.seq);
+      const index = items.findIndex(v => v.seq === seq);
+      items[index].active = !items[index].active;
+      this.setState({ items });
+    });
+
+    this.addEvent('click', '.filterBtn', ({ target }) => {
+      this.setState({ isFilter: Number(target.dataset.isFilter ) });
     });
   }
 }
