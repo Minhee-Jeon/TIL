93e1e0a1f7e1f6854e94564158e545a5dc254b8d
minhee <jmh3699@naver.com>
2022-11-05 17:02:50 +0900

    2-2 [컴포넌트 분할하기] component core, entry point 변경
    .
    ├── index.html
    └── src
        ├── App.js               # main에서 App 컴포넌트를 마운트한다.
        ├── main.js              # new entry 포인트
        ├── components
        │   ├── ItemAppender.js
        │   ├── ItemFilter.js
        │   └── Items.js               # ItemAppender, ItemFilter을 분리한다.
        └── core
            └── Component.js
    
    `src/core/Component.js`에 `$props` (부모 컴포넌트가 자식 컴포넌트에게 상태/메서드를 넘겨주기 위해) 와 `mounted`(render 이후에 추가적인 기능을 수행)를 추가한다.

--- javascript/[실습] Vanilla JS로 웹 컴포넌트 다루기/src/core/Component.js
+++ javascript/[실습] Vanilla JS로 웹 컴포넌트 다루기/src/core/Component.js
@@ -1,17 +1,22 @@
 export default class Component {
   $target;
+  $props;
   $state;
-  constructor ($target) {
+  constructor ($target, $props) {
     this.$target = $target;
+    this.$props = $props;
     this.setup();
     this.setEvent();
     this.render();
   }
   setup () {};
+  mounted () {};
   template () { return '';}
   render () {
     this.$target.innerHTML = this.template();
+    this.mounted();
   }
+  setEvent() {}
   setState (newState) {
     this.$state = { ...this.$state, ...newState };
     this.render();
