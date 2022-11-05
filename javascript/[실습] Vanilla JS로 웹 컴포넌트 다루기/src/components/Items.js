5a418879f201fed8945ac0644fd692cb89ba924f
minhee <jmh3699@naver.com>
2022-11-05 16:33:40 +0900

    1-3 [이벤트 처리]  이벤트 버블링 추상화
    이벤트 버블링을 통한 등록 과정을 메소드로 만들어 사용하면 코드가 더 깔끔해진다.

--- javascript/[실습] Vanilla JS로 웹 컴포넌트 다루기/src/components/Items.js
+++ javascript/[실습] Vanilla JS로 웹 컴포넌트 다루기/src/components/Items.js
@@ -20,18 +20,14 @@ export default class Items extends Component {
   }
 
   setEvent () {
-    // 모든 이벤트를 this.$target에 등록해 사용하면 된다.
-    this.$target.addEventListener('click', ({ target }) => {
+    this.addEvent('click', '.addBtn', ({ target }) => {
+      const { items } = this.$state;
+      this.setState({ items: [ ...items, `item${items.length + 1}` ] });
+    });
+    this.addEvent('click', '.deleteBtn', ({ target }) => {
       const items = [ ...this.$state.items ];
-
-      if (target.classList.contains('addBtn')) {
-        this.setState({ items: [ ...items, `item${items.length + 1}` ] });
-      }
-
-      if (target.classList.contains('deleteBtn')) {
-        items.splice(target.dataset.index, 1);
-        this.setState({ items });
-      }
+      items.splice(target.dataset.index, 1);
+      this.setState({ items });
     });
   }
 }
